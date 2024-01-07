87e306554b886ac99fc249be1e6656d844ef47f3
neimaaman <neimaaman1234@gmail.com>
2024-01-06 23:13:14 -0500

    added more css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html}"],
+  content: content: ["./src/**/*.{html,js}","./index.html}"],
   theme: {
     extend: {},
   },
