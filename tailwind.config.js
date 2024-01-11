c99ac99a5d8e0f4cbfd5ce609c4b983ea02014f4
neimaaman <neimaaman1234@gmail.com>
2024-01-10 21:25:15 -0500

    added css on the footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}","./index.html"],
   theme: {
     extend: {},
+    fontSize: {
+      xs: '0.7rem',
+      sm: '0.8rem'
+    }
   },
   plugins: [],
 }
+
+ 
 
